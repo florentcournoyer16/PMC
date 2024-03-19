fc2217c6e0c41f99e4b9474960c491fc15ce80ad
florentcournoyer16 <FlorentCournoyer@proton.me>
2024-03-18 22:49:08 -0400

    lmao

--- B_RNI_HLS/apc/src/RNI_2.cpp
+++ B_RNI_HLS/apc/src/RNI_2.cpp
@@ -20,13 +20,13 @@ void RNI (
 
 #pragma HLS INTERFACE mode=s_axilite port=return bundle=control
 
-	while (true)
+	while(true)
 	{
 		ap_axis< INPUT_LAYER_LENGHT * BASE_TYPE_LENGHT, 2, 5, 6 > input_buffer;
 		input_stream.read(input_buffer);
 
 		BASE_TYPE input_list[INPUT_LAYER_LENGHT];
-		for (BASE_TYPE i = 0; i < INPUT_LAYER_LENGHT; ++i)
+		for(BASE_TYPE i = 0; i < INPUT_LAYER_LENGHT; ++i)
 		{
 			input_list[i] = input_buffer.data.to_int() << BASE_TYPE_LENGHT * i;
 		}
@@ -39,14 +39,16 @@ void RNI (
 		output_layer(output_list);
 
 		ap_axis< OUTPUT_LAYER_LENGHT * BASE_TYPE_LENGHT, 2, 5, 6 > output_buffer;
-		for (BASE_TYPE i = 0; i < OUTPUT_LAYER_LENGHT; ++i)
+		for(BASE_TYPE i = 0; i < OUTPUT_LAYER_LENGHT; ++i)
 		{
 			output_buffer.data &= output_list[i] << BASE_TYPE_LENGHT * i;
 		}
 		output_stream.write(output_buffer);
 
-		if (input_buffer.last)
+		if(input_buffer.last)
+		{
 			break;
+		}
 	}
 }
 
@@ -61,10 +63,14 @@ void input_layer(BASE_TYPE input_list[INPUT_LAYER_LENGHT])
 			BASE_TYPE temp = NEURONS_MEMBRANE[neuron_index] + (WEIGHTS[weight_index] * input_list[weight_index]);
 			NEURONS_MEMBRANE[neuron_index] = temp;
 		}
-		if (NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
+		if(NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
 		{
 			NEURONS_STATE[neuron_index] = 1;
 			NEURONS_MEMBRANE[neuron_index] = RESET_MECHANISM_VALS[layer_index];
+		}
+		else
+		{
+			NEURONS_MEMBRANE[neuron_index] -= BETAS[layer_index];
 		}
 	}
 }
@@ -76,16 +82,20 @@ void inner_layer(BASE_TYPE layer_index)
 		WEIGHTS_LOOP: for(BASE_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index <  WEIGHTS_INDEX[neuron_index + 1]; ++weight_index)
 		{
 			BASE_TYPE neuron_state = NEURONS_STATE[NEURONS_INDEX[layer_index - 1] + weight_index - WEIGHTS_INDEX[neuron_index]];
-			if (neuron_state == 1)
+			if(neuron_state == 1)
 			{
 				BASE_TYPE temp = NEURONS_MEMBRANE[neuron_index] + WEIGHTS[weight_index];
 				NEURONS_MEMBRANE[neuron_index] = temp;
 			}
 		}
-		if (NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
+		if(NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
 		{
 			NEURONS_STATE[neuron_index] = 1;
 			NEURONS_MEMBRANE[neuron_index] = RESET_MECHANISM_VALS[layer_index];
+		}
+		else
+		{
+			NEURONS_MEMBRANE[neuron_index] -= BETAS[layer_index];
 		}
 	}
 
@@ -103,16 +113,20 @@ void output_layer(BASE_TYPE output_list[OUTPUT_LAYER_LENGHT])
 		WEIGHTS_LOOP: for(BASE_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index <  WEIGHTS_INDEX[neuron_index + 1]; ++weight_index)
 		{
 			BASE_TYPE neuron_state = NEURONS_STATE[NEURONS_INDEX[layer_index - 1] + weight_index - WEIGHTS_INDEX[neuron_index]];
-			if (neuron_state == 1)
+			if(neuron_state == 1)
 			{
 				BASE_TYPE temp = NEURONS_MEMBRANE[neuron_index] + WEIGHTS[weight_index];
 				NEURONS_MEMBRANE[neuron_index] = temp;
 			}
 		}
-		if (NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
+		if(NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
 		{
 			NEURONS_MEMBRANE[neuron_index] = RESET_MECHANISM_VALS[layer_index];
 			output_list[neuron_index % OUTPUT_LAYER_LENGHT] = 1;
+		}
+		else
+		{
+			NEURONS_MEMBRANE[neuron_index] -= BETAS[layer_index];
 		}
 	}
 
