afe4ea671d1e3869e88572a09e5340d336b987f3
FlorentCournoyer <FlorentCournoyer@proton.me>
2024-10-03 18:39:09 -0400

    la je commit fr fr

--- A_SCRIPTS/model/output/model_LIGHT_SENSOR_3.cpp
+++ A_SCRIPTS/model/output/model_LIGHT_SENSOR_3.cpp
@@ -1,12 +1,22 @@
-
 #include "../inc/model_LIGHT_SENSOR_3.h"
 
-void input_layer(pkt input_pkts[INPUT_LENGHT]);
-void inner_layer_1();
-void inner_layer_2();
-void inner_layer_3();
-void output_layer();
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
 
+void input_layer(pkt input_pkts[INPUT_LENGHT]);
+void inner_layer_1(void);
+void inner_layer_2(void);
+void inner_layer_3(void);
+void output_layer(void);
+
+void leak_neuron(INDEX_TYPE layer_index, INDEX_TYPE neuron_index);
+void reset_neuron_membrane(INDEX_TYPE layer_index, INDEX_TYPE neuron_index);
+void update_neuron_state_reset_membrane(INDEX_TYPE layer_index);
+void update_membrane_probe(INDEX_TYPE neuron_index);
+
+void write_probe_file(void);
+std::ofstream probe_file;
 
 void RNI(pkt_stream& in_stream, pkt_stream& out_stream)
 {
@@ -18,10 +28,11 @@ void RNI(pkt_stream& in_stream, pkt_stream& out_stream)
 	pkt in_pkts[INPUT_LENGHT];
 	pkt out_pkts[OUTPUT_LENGHT];
 
+	probe_file.open(MEMBRANE_PROBE_OUTPUT_FILEPATH);
 
 	while(true)
 	{
-		for(int i = 0; i < INPUT_LENGHT; i++)
+		for(INDEX_TYPE i = 0; i < INPUT_LENGHT; i++)
 			in_stream.read(in_pkts[i]);
 
 		input_layer(in_pkts);
@@ -34,131 +45,165 @@ void RNI(pkt_stream& in_stream, pkt_stream& out_stream)
 
 		for(INDEX_TYPE i = 0; i < NEURONS_MEMBRANE_LENGHT; i++)
 			out_pkts[i] = in_pkts[0];
-			out_pkts[i].data = NEURONS_MEMBRANE[i]
-			output_stream.write([i]);
+			out_pkts[i].data = NEURONS_MEMBRANE[i];
+			output_stream.write(out_pkts[i]);
 
 
 		if(input_buffer.last)
 			break;
 	}
 
+	write_probe_file();
+	probe_file.close();
+
     return;
 }
+
 
 void input_layer(pkt input_pkts[INPUT_LENGHT])
 {
 	INDEX_TYPE layer_index = 0;
-	NEURONS_LOOP: for(INDEX_TYPE neuron_index = NEURONS_INDEX[layer_index]; neuron_index < NEURONS_INDEX[layer_index + 1];  neuron_index++)
+	NEURONS_LOOP_0: for(INDEX_TYPE neuron_index = NEURONS_INDEX[layer_index]; neuron_index < NEURONS_INDEX[layer_index + 1];  neuron_index++)
 	{
-		MEMBRANE_TYPE membrane_sign = NEURONS_MEMBRANE[neuron_index] & 0x100;
-		NEURONS_MEMBRANE[neuron_index] = (NEURONS_MEMBRANE[neuron_index] >> 1 + NEURONS_MEMBRANE[neuron_index] >> 2 + NEURONS_MEMBRANE[neuron_index] >> 4) || membrane_sign;
-		WEIGHTS_LOOP: for(INDEX_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index < WEIGHTS_INDEX[neuron_index + 1]; weight_index++)
+		leak_neuron(layer_index, neuron_index);
+
+		WEIGHTS_LOOP_0: for(INDEX_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index < WEIGHTS_INDEX[neuron_index + 1]; weight_index++)
 		{
 			NEURONS_MEMBRANE[neuron_index] += WEIGHTS[weight_index] * input_pkts[weight_index % INPUT_LENGHT].data;
 		}
-
-        if(NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
-        {
-            NEURONS_STATE[neuron_index] = 1;
-            NEURONS_MEMBRANE[neuron_index] = RESET[layer_index];
-        }
+		update_membrane_probe(layer_index);
+        reset_neuron_membrane(layer_index, neuron_index);
     }
 }
+
 
 void inner_layer_1(void)
 {
 	INDEX_TYPE layer_index = 1;
-    NEURONS_LOOP: for(INDEX_TYPE neuron_index = NEURONS_INDEX[layer_index]; neuron_index < NEURONS_INDEX[layer_index + 1];  neuron_index++)
+    NEURONS_LOOP_1: for(INDEX_TYPE neuron_index = NEURONS_INDEX[layer_index]; neuron_index < NEURONS_INDEX[layer_index + 1];  neuron_index++)
 	{
-		MEMBRANE_TYPE sign = NEURONS_MEMBRANE[neuron_index] & 0x100;
-		NEURONS_MEMBRANE[neuron_index] = (NEURONS_MEMBRANE[neuron_index] >> 1 + NEURONS_MEMBRANE[neuron_index] >> 2 + NEURONS_MEMBRANE[neuron_index] >> 4)  || sign;
-		WEIGHTS_LOOP: for(INDEX_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index <  WEIGHTS_INDEX[neuron_index + 1]; weight_index++)
+		leak_neuron(neuron_index, layer_index);
+		WEIGHTS_LOOP_1: for(INDEX_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index <  WEIGHTS_INDEX[neuron_index + 1]; weight_index++)
 		{
-			ap_int< 2 > neuron_state = NEURONS_STATE[NEURONS_INDEX[layer_index - 1] + weight_index - WEIGHTS_INDEX[neuron_index]];
+			STATE_TYPE neuron_state = NEURONS_STATE[NEURONS_INDEX[layer_index - 1] + weight_index - WEIGHTS_INDEX[neuron_index]];
 			if(neuron_state == 1)
 				NEURONS_MEMBRANE[neuron_index] += WEIGHTS[weight_index];
 		}
-
-        if(NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
-		{
-			NEURONS_STATE[neuron_index] = 1;
-			NEURONS_MEMBRANE[neuron_index] = RESET[layer_index];
-		}
+        update_membrane_probe(neuron_index);
+        reset_neuron_membrane(layer_index, neuron_index);
 	}
-	NEURONS_STATE_RESET_LOOP: for(INDEX_TYPE neuron_state_index = NEURONS_INDEX[layer_index - 1]; neuron_state_index < NEURONS_INDEX[layer_index];  neuron_state_index++)
-		NEURONS_STATE[neuron_state_index] = 0;
+	update_neuron_state_reset_membrane(layer_index-1);
 }
+
 
 void inner_layer_2(void)
 {
 	INDEX_TYPE layer_index = 2;
-    NEURONS_LOOP: for(INDEX_TYPE neuron_index = NEURONS_INDEX[layer_index]; neuron_index < NEURONS_INDEX[layer_index + 1];  neuron_index++)
+    NEURONS_LOOP_2: for(INDEX_TYPE neuron_index = NEURONS_INDEX[layer_index]; neuron_index < NEURONS_INDEX[layer_index + 1];  neuron_index++)
 	{
-		MEMBRANE_TYPE sign = NEURONS_MEMBRANE[neuron_index] & 0x100;
-		NEURONS_MEMBRANE[neuron_index] = (NEURONS_MEMBRANE[neuron_index] >> 1 + NEURONS_MEMBRANE[neuron_index] >> 2 + NEURONS_MEMBRANE[neuron_index] >> 4)  || sign;
-		WEIGHTS_LOOP: for(INDEX_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index <  WEIGHTS_INDEX[neuron_index + 1]; weight_index++)
+		leak_neuron(layer_index, neuron_index);
+		WEIGHTS_LOOP_2: for(INDEX_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index <  WEIGHTS_INDEX[neuron_index + 1]; weight_index++)
 		{
-			ap_int< 2 > neuron_state = NEURONS_STATE[NEURONS_INDEX[layer_index - 1] + weight_index - WEIGHTS_INDEX[neuron_index]];
+			STATE_TYPE neuron_state = NEURONS_STATE[NEURONS_INDEX[layer_index - 1] + weight_index - WEIGHTS_INDEX[neuron_index]];
 			if(neuron_state == 1)
 				NEURONS_MEMBRANE[neuron_index] += WEIGHTS[weight_index];
 		}
-
-        if(NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
-		{
-			NEURONS_STATE[neuron_index] = 1;
-			NEURONS_MEMBRANE[neuron_index] = RESET[layer_index];
-		}
+        update_membrane_probe(neuron_index);
+        reset_neuron_membrane(layer_index, neuron_index);
 	}
-	NEURONS_STATE_RESET_LOOP: for(INDEX_TYPE neuron_state_index = NEURONS_INDEX[layer_index - 1]; neuron_state_index < NEURONS_INDEX[layer_index];  neuron_state_index++)
-		NEURONS_STATE[neuron_state_index] = 0;
+	update_neuron_state_reset_membrane(layer_index-1);
 }
+
 
 void inner_layer_3(void)
 {
 	INDEX_TYPE layer_index = 3;
-    NEURONS_LOOP: for(INDEX_TYPE neuron_index = NEURONS_INDEX[layer_index]; neuron_index < NEURONS_INDEX[layer_index + 1];  neuron_index++)
+    NEURONS_LOOP_3: for(INDEX_TYPE neuron_index = NEURONS_INDEX[layer_index]; neuron_index < NEURONS_INDEX[layer_index + 1];  neuron_index++)
 	{
-		MEMBRANE_TYPE sign = NEURONS_MEMBRANE[neuron_index] & 0x100;
-		NEURONS_MEMBRANE[neuron_index] = (NEURONS_MEMBRANE[neuron_index] >> 1 + NEURONS_MEMBRANE[neuron_index] >> 2 + NEURONS_MEMBRANE[neuron_index] >> 4)  || sign;
-		WEIGHTS_LOOP: for(INDEX_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index <  WEIGHTS_INDEX[neuron_index + 1]; weight_index++)
+		leak_neuron(layer_index, neuron_index);
+
+		WEIGHTS_LOOP_3: for(INDEX_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index <  WEIGHTS_INDEX[neuron_index + 1]; weight_index++)
 		{
-			ap_int< 2 > neuron_state = NEURONS_STATE[NEURONS_INDEX[layer_index - 1] + weight_index - WEIGHTS_INDEX[neuron_index]];
+			STATE_TYPE neuron_state = NEURONS_STATE[NEURONS_INDEX[layer_index - 1] + weight_index - WEIGHTS_INDEX[neuron_index]];
 			if(neuron_state == 1)
 				NEURONS_MEMBRANE[neuron_index] += WEIGHTS[weight_index];
 		}
-
-        if(NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
-		{
-			NEURONS_STATE[neuron_index] = 1;
-			NEURONS_MEMBRANE[neuron_index] = RESET[layer_index];
-		}
+		update_membrane_probe(neuron_index);
+        reset_neuron_membrane(layer_index, neuron_index);
 	}
-	NEURONS_STATE_RESET_LOOP: for(INDEX_TYPE neuron_state_index = NEURONS_INDEX[layer_index - 1]; neuron_state_index < NEURONS_INDEX[layer_index];  neuron_state_index++)
+	update_neuron_state_reset_membrane(layer_index-1);
+}
+
+
+void output_layer(void)
+{
+	INDEX_TYPE layer_index = NEURONS_INDEX_LENGHT - 2;
+	NEURONS_LOOP_4: for(INDEX_TYPE neuron_index = NEURONS_INDEX[layer_index]; neuron_index < NEURONS_INDEX[layer_index + 1];  neuron_index++)
+	{
+		leak_neuron(layer_index, neuron_index);
+
+		WEIGHTS_LOOP_4: for(INDEX_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index <  WEIGHTS_INDEX[neuron_index + 1]; weight_index++)
+		{
+			STATE_TYPE neuron_state = NEURONS_STATE[NEURONS_INDEX[layer_index - 1] + weight_index - WEIGHTS_INDEX[neuron_index]];
+			if(neuron_state == 1)
+				NEURONS_MEMBRANE[neuron_index] += WEIGHTS[weight_index];
+		}
+        update_membrane_probe(neuron_index);
+        reset_neuron_membrane(layer_index, neuron_index);
+	}
+
+	update_neuron_state_reset_membrane(layer_index-1);
+}
+
+
+void leak_neuron(INDEX_TYPE layer_index, INDEX_TYPE neuron_index)
+{
+	MEMBRANE_TYPE sign = NEURONS_MEMBRANE[neuron_index] & 0x100;
+	MEMBRANE_TYPE membrane_leak_accumulator = 0x0;
+	NEURON_LEAK_LOOP: for(INDEX_TYPE beta_index = 1; beta_index < BETAS[layer_index]; beta_index++) {
+		membrane_leak_accumulator += NEURONS_MEMBRANE[neuron_index] >> 1 * beta_index;
+	}
+	NEURONS_MEMBRANE[neuron_index] = membrane_leak_accumulator || sign;
+}
+
+
+void reset_neuron_membrane(INDEX_TYPE layer_index, INDEX_TYPE neuron_index)
+{
+	if(NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
+	{
+		NEURONS_STATE[neuron_index] = 1;
+		NEURONS_MEMBRANE[neuron_index] = RESET[layer_index];
+	}
+}
+
+
+void update_neuron_state_reset_membrane(INDEX_TYPE layer_index)
+{
+	NEURONS_STATE_RESET_LOOP: for(INDEX_TYPE neuron_state_index = NEURONS_INDEX[layer_index]; neuron_state_index < NEURONS_INDEX[layer_index];  neuron_state_index++)
 		NEURONS_STATE[neuron_state_index] = 0;
 }
 
 
-void output_layer()
+void update_membrane_probe(INDEX_TYPE neuron_index)
 {
-	INDEX_TYPE layer_index = NEURONS_INDEX_LENGHT - 2;
-	NEURONS_LOOP: for(INDEX_TYPE neuron_index = NEURONS_INDEX[layer_index]; neuron_index < NEURONS_INDEX[layer_index + 1];  neuron_index++)
+	if(neuron_index == MEMBRANE_PROBE_NEURON_INDEX)
 	{
-		MEMBRANE_TYPE sign = NEURONS_MEMBRANE[neuron_index] & 0x100;
-		NEURONS_MEMBRANE[neuron_index] = (NEURONS_MEMBRANE[neuron_index] >> 1 + NEURONS_MEMBRANE[neuron_index] >> 2 + NEURONS_MEMBRANE[neuron_index] >> 4) || sign;
-		WEIGHTS_LOOP: for(INDEX_TYPE weight_index = WEIGHTS_INDEX[neuron_index]; weight_index <  WEIGHTS_INDEX[neuron_index + 1]; weight_index++)
-		{
-			ap_int< 2 > neuron_state = NEURONS_STATE[NEURONS_INDEX[layer_index - 1] + weight_index - WEIGHTS_INDEX[neuron_index]];
-			if(neuron_state == 1)
-				NEURONS_MEMBRANE[neuron_index] += WEIGHTS[weight_index];
-		}
-    
-        if(NEURONS_MEMBRANE[neuron_index] > THRESHOLDS[layer_index])
-		{
-			NEURONS_STATE[neuron_index] = 1;
-			NEURONS_MEMBRANE[neuron_index] = RESET[layer_index];
-		}
+		if(MEMBRANE_PROBE_NEURON_INDEX == MEMBRANE_PROBE_LENGHT-1)
+			write_probe_file();
+		MEMBRANE_PROBE[MEMBRANE_PROBE_CURRENT_INDEX] = NEURONS_MEMBRANE[neuron_index];
+		MEMBRANE_PROBE_CURRENT_INDEX++; 
 	}
+}
 
-	NEURONS_STATE_RESET_LOOP: for(INDEX_TYPE neuron_state_index = NEURONS_INDEX[layer_index - 1]; neuron_state_index < NEURONS_INDEX[layer_index];  neuron_state_index++)
-		NEURONS_STATE[neuron_state_index] = 0;
+
+void write_probe_file(void)
+{
+    if (&probe_file == NULL)
+    {
+        std::cout << "Error with probe file" << std::endl;
+        return;
+    }
+    for(INDEX_TYPE i = 0; i < MEMBRANE_PROBE_CURRENT_INDEX; i++)
+        probe_file << MEMBRANE_PROBE[i] << ",\n";
+    MEMBRANE_PROBE_CURRENT_INDEX = 0;
 }
