db99ab2b58083080f3a4bcff1bed5a1c79b996f1
FlorentCournoyer <FlorentCournoyer@proton.me>
2024-11-14 13:34:17 -0500

    removed only leak positive

--- A_RNI_SCRIPTS/model/code_writer.py
+++ A_RNI_SCRIPTS/model/code_writer.py
@@ -327,14 +327,11 @@ def __append_leak_neuron_definition__(code_segments):
     code_segments.append("""
 void leak_neuron(INDEX_TYPE layer_index, INDEX_TYPE neuron_index)
 {
-    if (NEURONS_MEMBRANE[neuron_index] > 0)
-    {
-        MEMBRANE_TYPE membrane_leak_accumulator = 0x0;
-        NEURON_LEAK_LOOP: for(INDEX_TYPE beta_index = 1; beta_index < BETAS[layer_index]; beta_index++) {
-            membrane_leak_accumulator += NEURONS_MEMBRANE[neuron_index] / hls::pow(2, beta_index);
-        }
-        NEURONS_MEMBRANE[neuron_index] = membrane_leak_accumulator;
+    MEMBRANE_TYPE membrane_leak_accumulator = 0x0;
+    NEURON_LEAK_LOOP: for(INDEX_TYPE beta_index = 1; beta_index < BETAS[layer_index]; beta_index++) {
+        membrane_leak_accumulator += NEURONS_MEMBRANE[neuron_index] / hls::pow(2, beta_index);
     }
+    NEURONS_MEMBRANE[neuron_index] = membrane_leak_accumulator;
 }
 
 """)
