55ce1a4f8260bbf5868e275fdd22491b6cad290a
NeuroSOC_PMC <NeuroSOC_PMC@proton.me>
2024-11-15 13:38:03 -0500

    src_all

--- src_all.sh
+++ src_all.sh
@@ -1,2 +1,4 @@
 alias RNI_parser="python3 A_RNI_SCRIPTS/run_model_parser.py"
 alias RNI_plotter="python3 A_RNI_SCRIPTS/data/run_inout_plotter.py"
+alias RNI_comp_plotter="python3 A_RNI_SCRIPTS/data/run_compraison_plotter.py"
+alias RNI_ubi_plotter="python3 A_RNI_SCRIPTS/data/run_ubiquitous_plotter.py -f output"
